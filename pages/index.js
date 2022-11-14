912c34d18a9b4f8159d0c3058e38a0cd3fc56123
Nicholas <nic.schneider@ymail.com>
2022-11-14 09:13:47 +0100

    add about

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { About } from "@components/sections/About";
 import { Portfolio } from "@components/sections/Portfolio/index";
 import { Layout } from "src/components/Layout";
 import Hero from "src/components/sections/Hero";
@@ -8,6 +9,7 @@ export default function Home() {
             <Hero />
 
             <main>
+                <About />
                 <Portfolio />
             </main>
         </Layout>
