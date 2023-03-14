6acbf801a327f47f9e169191971a6560de9543c1
Nicholas <nic.schneider@ymail.com>
2023-03-14 20:31:51 +0100

    add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,13 +20,16 @@ export const Layout = ({ children, title }) => {
                 <meta name="author" content="Nicholas Schneider" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>{`${title ? title + " | " : ""}Nicholas Schneider`}</title>
-                {/* FAVICON */}
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </Head>
 
             <GlobalStyle />
             <Loading />
             <Background />
-            <Cursor />
+            {/* <Cursor /> */}
 
             <Navbar />
             <SideInfo pos={"right"}>
@@ -58,5 +61,12 @@ export const Container = styled.main`
     /* min-height: 100vh; */
     @media (max-width: 768px) {
         padding: var(--padding-mobile);
+    }
+`;
+
+export const LowLevelSiteContainer = styled.div`
+    padding-top: 200px;
+    * {
+        text-align: justify;
     }
 `;
