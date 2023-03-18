9fcd6cfce51b1d76eba56c53b9e25a4e02d15868
Nicholas <nic.schneider@ymail.com>
2023-03-18 19:32:53 +0100

    wordcycler prototype

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,7 +1,8 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { FaPaperPlane } from "react-icons/fa";
 import { CtaBtn } from "@components/CtaBtn";
 import { textOnBg, fade } from "src/styles/variables";
+import { useState, useEffect } from "react";
 
 const HeroSection = styled.section`
     min-height: 100vh;
@@ -23,7 +24,7 @@ const HeroSection = styled.section`
     }
     h1 {
         font-size: clamp(50px, 9vw, 90px);
-        color: var(--white);
+        color: var(--light-slate);
         font-weight: 800;
         ${fade(6)};
     }
@@ -42,12 +43,56 @@ const HeroSection = styled.section`
     }
 `;
 
+const breatheAnimation = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateX(-10px);
+    }
+    10% {
+        opacity: 1;
+        transform: translateX(0px);
+    }
+    90% {
+        opacity: 1;
+        transform: translateX(0px);
+    }
+    100% {
+        opacity: 0;
+        transform: translateX(30px);
+    }
+`;
+
+const StyledWord = styled.span`
+    animation-name: ${breatheAnimation};
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: forwards;
+    animation-duration: 3000ms;
+    color: var(--white);
+    display: inline-block;
+`;
+
+const WordCycler = ({ words }) => {
+    const [currentWordIndex, setCurrentWordIndex] = useState(0);
+    const [currentWord, setCurrentWord] = useState(words[currentWordIndex]);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentWordIndex((prevIndex) => (prevIndex === words.length - 1 ? 0 : prevIndex + 1));
+            setCurrentWord(words[currentWordIndex]);
+        }, 3000);
+
+        return () => clearInterval(intervalId);
+    }, [currentWordIndex]);
+
+    return <StyledWord key={currentWordIndex}>{currentWord}.</StyledWord>;
+};
+
 export const Hero = () => {
+    const words = ["Webseiten", "Web-Apps", "Interaktivität", "Frontends", "Backends"];
     return (
         <HeroSection id="start">
             <h3>Hallo, ich bin Nicholas</h3>
-            {/* <h1>Nicholas</h1> */}
-            <h1>Ich baue Webseiten.</h1>
+            <h1>Ich baue {<WordCycler words={words} />}</h1>
             <p>Ich bin Frontend (und manchmal auch Backend) Entwickler aus Saarbrücken, spezialisiert auf&apos;s Bauen und Entwerfen von Webseiten, Applikationen und allem, was dazwischen liegt.</p>
             <CtaBtn href={"mailto:hallo@nicholas-schneider.de"}>
                 Sag hallo!
