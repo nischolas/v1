d5392b69807fd326f4cca2e2f433ee639d7aecb2
Nicholas <nic.schneider@ymail.com>
2023-04-16 22:26:42 +0200

    ios theme color

--- pages/_app.js
+++ pages/_app.js
@@ -45,6 +45,9 @@ function MyApp({ Component, pageProps }) {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
                 <link rel="manifest" href="/favicon/site.webmanifest" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="theme-color" content="#101419" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
             </Head>
             <GlobalStyle />
             {loading && isOnHp ? <Loading loadingDuration={loadingDuration} /> : <Component {...pageProps} />}
