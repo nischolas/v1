b16665d110b8eb2fd4b74e8272ac55fb3f09b841
Nicholas Mathieu <nic.schneider@ymail.com>
2026-06-07 21:38:09 +0200

    typo

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -92,7 +92,7 @@ const WordCycler = ({ words }) => {
 export const Hero = () => {
     const isMobile = useIsViewportLessThanWidth();
     // max 14 chars
-    const words = ["Webs-Apps", "Landingpages", "Webseiten", "Interaktivität", "Lösungen", "Tools"];
+    const words = ["Web-Apps", "Landingpages", "Webseiten", "Interaktivität", "Lösungen", "Tools"];
     return (
         <HeroSection id="start">
             <h3>Hallo, ich bin Nicholas</h3>
